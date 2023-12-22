85931729ef4c0ba7797269597a6fe5ae85cacc6e
Erik Barroso <barroso.erik@live.com>
2023-12-22 16:14:16 -0300

    when

---
+++ src/When.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val valor = 3
+
+    when(valor) {
+
+        1 -> println("um")
+
+        2 -> println("dois")
+
+        3 -> println("três")
+
+        else -> println("Não achamos o $valor")
+    }
+}
