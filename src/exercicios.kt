4dd75924d1988456fa8da3f66cd0d2a8689ade35
Erik Barroso <coderik1996@gmail.com>
2023-12-26 12:57:52 -0300

    potência de dois

--- src/exercicios.kt
+++ src/exercicios.kt
@@ -1,19 +1,34 @@
 import java.util.Scanner
 fun main() {
+
+    // operaçao entre 2 números
+//    val reader = Scanner(System.`in`)
+//    // Escreva seu código aqui
+//
+//    val operacao: String = reader.next()
+//    val a: Int = reader.nextInt()
+//    val b: Int = reader.nextInt()
+//
+//    println(
+//        when{
+//            operacao == "+" -> a + b
+//            operacao == "-" -> a - b
+//            operacao == "*" -> a * b
+//            operacao == "/" -> "%.1f".format (a.toDouble() / b)
+//            else -> "Invalid"
+//        }
+//    )
+
+    // potência de dois
     val reader = Scanner(System.`in`)
-    // Escreva seu código aqui
 
-    val operacao: String = reader.next()
-    val a: Int = reader.nextInt()
-    val b: Int = reader.nextInt()
+    var result: Int = 1
+    val n: Int = reader.nextInt()
 
-    println(
-        when{
-            operacao == "+" -> a + b
-            operacao == "-" -> a - b
-            operacao == "*" -> a * b
-            operacao == "/" -> "%.1f".format (a.toDouble() / b)
-            else -> "Invalid"
-        }
-    )
+    for(number in 1..n){
+        result *= 2
+    }
+    print(result)
 }
+
+
