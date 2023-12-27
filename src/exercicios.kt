f523d79717296d29c29d5a6b795f84a37f5ee5fa
Erik Barroso <coderik1996@gmail.com>
2023-12-27 07:41:06 -0300

    As desventuras de Snake, expressão aritimética

--- src/exercicios.kt
+++ src/exercicios.kt
@@ -1,34 +1,39 @@
 import java.util.Scanner
 fun main() {
 
-    // operaçao entre 2 números
-//    val reader = Scanner(System.`in`)
-//    // Escreva seu código aqui
-//
-//    val operacao: String = reader.next()
-//    val a: Int = reader.nextInt()
-//    val b: Int = reader.nextInt()
-//
-//    println(
-//        when{
-//            operacao == "+" -> a + b
-//            operacao == "-" -> a - b
-//            operacao == "*" -> a * b
-//            operacao == "/" -> "%.1f".format (a.toDouble() / b)
-//            else -> "Invalid"
-//        }
-//    )
+        // operaçao entre 2 números
+    //    val reader = Scanner(System.`in`)
+    //    // Escreva seu código aqui
+    //
+    //    val operacao: String = reader.next()
+    //    val a: Int = reader.nextInt()
+    //    val b: Int = reader.nextInt()
+    //
+    //    println(
+    //        when{
+    //            operacao == "+" -> a + b
+    //            operacao == "-" -> a - b
+    //            operacao == "*" -> a * b
+    //            operacao == "/" -> "%.1f".format (a.toDouble() / b)
+    //            else -> "Invalid"
+    //        }
+    //    )
 
     // potência de dois
-    val reader = Scanner(System.`in`)
+//    val reader = Scanner(System.`in`)
+    //
+    //var result: Int = 1
+    //val n: Int = reader.nextInt()
+    //
+    //for(number in 1..n){
+    //    result *= 2
+    //}
+    //print(result)
+
+    //
+
 
     var result: Int = 1
-    val n: Int = reader.nextInt()
-
-    for(number in 1..n){
-        result *= 2
-    }
-    print(result)
 }
 
 
---
+++ src/exercícios_expressao_aritimeticas.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+
+fun main (){
+    val reader = Scanner(System.`in`)
+
+    // cada e = -3 q . Qual a quantidade total de q?
+    val q1 = reader.nextInt()
+    val q2 = reader.nextInt()
+    val q3 = reader.nextInt()
+    val e1 = reader.nextInt()
+    val e2 = reader.nextInt()
+    val e3 = reader.nextInt()
+
+    print((q1 + q2 + q3) - ((e1 + e2 + e3) *3) )
+
+}
+
