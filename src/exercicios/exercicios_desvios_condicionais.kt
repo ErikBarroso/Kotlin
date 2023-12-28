9506a5310848c12f9fc4ddc927b74b7d11dbcb58
Erik Barroso <coderik1996@gmail.com>
2023-12-28 05:51:54 -0300

    multiplos de 7

--- src/exercicios/exercicios_desvios_condicionais.kt
+++ src/exercicios/exercicios_desvios_condicionais.kt
@@ -7,14 +7,26 @@ fun main() {
 
     //alunos de até 4 letras = Grupo A, 5 a 10 letras = grupo b, + de 10 letras = grupo c
 
-    val aluno: String = reader.next()
+//    val aluno: String = reader.next()
+//
+//    val grupo = println(
+//        when {
+//        aluno.length < 5 -> "GRUPO A"
+//        aluno.length < 11 -> "GRUPO B"
+//        aluno.length > 10 -> "GRUPO C"
+//        else -> "invalid"
+//        }
+//    )
 
-    val grupo = println(
-        when {
-        aluno.length < 5 -> "GRUPO A"
-        aluno.length < 11 -> "GRUPO B"
-        aluno.length > 10 -> "GRUPO C"
-        else -> "invalid"
+    // multiplos de 7
+
+    val numero = reader.nextInt()
+
+    println(
+        when{
+            numero % 7 == 0 -> "É MÚLTIPLO DE 7"
+            numero % 7 != 0 -> "ESSE NÃO É"
+            else -> "invalid"
         }
     )
 }
