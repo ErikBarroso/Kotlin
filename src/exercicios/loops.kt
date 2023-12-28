95b8f3db8f14f8e3c488fdcf1c3edd21b0744b91
Erik Barroso <coderik1996@gmail.com>
2023-12-28 07:49:33 -0300

    invertendo números

--- src/exercicios/loops.kt
+++ src/exercicios/loops.kt
@@ -4,5 +4,14 @@ import java.util.Scanner
 fun main() {
     var reader = Scanner(System.`in`)
 
+    // invertendo números
 
+    while (true) {
+        var numero = reader.nextInt()
+        if (numero != 0) {
+            println(numero*-1)
+        }else {
+            break
+        }
+    }
 }
