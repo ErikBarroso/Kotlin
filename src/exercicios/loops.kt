ac9e5e77e86abaad83ac802fe7e1af2d182d5df0
Erik Barroso <coderik1996@gmail.com>
2023-12-29 06:18:28 -0300

    somando números

--- src/exercicios/loops.kt
+++ src/exercicios/loops.kt
@@ -6,12 +6,22 @@ fun main() {
 
     // invertendo números
 
-    while (true) {
-        var numero = reader.nextInt()
-        if (numero != 0) {
-            println(numero*-1)
-        }else {
-            break
-        }
+//    while (true) {
+//        var numero = reader.nextInt()
+//        if (numero != 0) {
+//            println(numero*-1)
+//        }else {
+//            break
+//        }
+//    }
+
+    // Somando números
+
+    val quantidadeNumeros = reader.nextInt()
+    var soma = 0
+
+    for(i in 0 until quantidadeNumeros) {
+        soma += reader.nextInt()
     }
+    print(soma)
 }
