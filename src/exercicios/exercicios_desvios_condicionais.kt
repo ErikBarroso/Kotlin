72bc2a63970e402c2932c9ac8923ec3b0bf1ed72
Erik Barroso <coderik1996@gmail.com>
2023-12-29 05:52:42 -0300

    classificação futebol

--- src/exercicios/exercicios_desvios_condicionais.kt
+++ src/exercicios/exercicios_desvios_condicionais.kt
@@ -32,17 +32,40 @@ fun main() {
 
     //Saída diferente a depender do valor de P e R
 
-    val p = reader.nextInt()
-    val r = reader.nextInt()
+//    val p = reader.nextInt()
+//    val r = reader.nextInt()
+//
+//    println(
+//        when {
+//            p == 0 -> "C"
+//            p == 1 && r == 1 -> "A"
+//            p == 1 && r == 0 -> "B"
+//            else -> "invalid"
+//        }
+//    )
+
+    // Classificação futebol
+
+    val time1Vitorias = reader.nextInt()
+    val time1Empates = reader.nextInt()
+    val time1Gols = reader.nextInt()
+    val time2Vitorias = reader.nextInt()
+    val time2Empates = reader.nextInt()
+    val time2Gols = reader.nextInt()
+
+    val pontosTime1 = (time1Vitorias * 3) + time1Empates
+    val pontosTime2 = (time2Vitorias * 3) + time2Empates
 
     println(
         when {
-            p == 0 -> "C"
-            p == 1 && r == 1 -> "A"
-            p == 1 && r == 0 -> "B"
-            else -> "invalid"
+            pontosTime1 > pontosTime2 -> "Time 1 vencedor"
+            pontosTime1 < pontosTime2 -> "Time 2 vencedor"
+            time1Gols > time2Gols -> "Time 1 vencedor"
+            time1Gols < time2Gols -> "Time 2 vencedor"
+            else -> "="
         }
     )
+
 
 
 }
